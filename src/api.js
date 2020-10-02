802440052694a3ff4cb6ab6eeb12be571f19ef92
mhmh55516 <mhmh55516@gmail.com>
2020-10-02 18:59:54 +0200

    Update api.js

--- src/api.js
+++ src/api.js
@@ -3,14 +3,21 @@ const serverless = require('serverless-http');
 const cors = require('cors');
 const app = express();
 //app.use(cors);
-const router = express.Router();
+//const router = express.Router();
 
 router.get('/',(req,res)=>{
     res.json({
         'hello':'hi from node server'
+        
     })
 })
 
 app.use('/.netlify/functions/api',router);
+app.use('/api',(req,res)=>{
+    res.json({
+        'hello':'hi from node server'
+        
+    })
+})
 //module.exports = app;
-module.exports.handler = serverless(app);
+//module.exports.handler = serverless(app);
